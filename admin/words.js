a3dead0c44f7b6d741027eb6cc224a630a72a983
Tobias Kronschnabl <Tobias.Kronschnabl@msg.group>
2022-07-06 07:10:53 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'pv-manager adapter settings': {
+        'en': 'Adapter settings for pv-manager',
+        'de': 'Adaptereinstellungen für pv-manager',
+        'ru': 'Настройки адаптера для pv-manager',
+        'pt': 'Configurações do adaptador para pv-manager',
+        'nl': 'Adapterinstellingen voor pv-manager',
+        'fr': "Paramètres d'adaptateur pour pv-manager",
+        'it': "Impostazioni dell'adattatore per pv-manager",
+        'es': 'Ajustes del adaptador para pv-manager',
+        'pl': 'Ustawienia adaptera dla pv-manager',
+        'zh-cn': 'pv-manager的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
